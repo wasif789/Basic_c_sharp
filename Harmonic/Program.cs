95bbb86c73790bac2748bb26388d3539a7736630
wasif farid <wsffarid1@gmail.com>
2021-09-03 15:00:37 +0530

    harmonic added

---
+++ Harmonic/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Harmonic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i,N;
+            Console.WriteLine("enter the harmonic value: ");
+            N = Convert.ToInt32(Console.ReadLine());
+            for (i = 0; i <= N; i++)
+            {
+                if (i == N)
+                {
+                    Console.Write("1/" + i);
+                }
+                else
+                {
+                    Console.Write("1/" + i + "+");
+                }
+            }
+
+        }
+    }
+}
