17e12d768ad1eec8330b20221ec1a290156d5cb3
wasif farid <wsffarid1@gmail.com>
2021-09-03 14:21:17 +0530

    power of 2 added

---
+++ PowerOf2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PowerOf2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the power till you want the table of 2");
+            int N = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i <= N; i++)
+            {
+                Console.WriteLine("2 to the power of {0} is"+Math.Pow(2, i),i);
+            }
+        }
+    }
+}
