91037835c5c047245e1a9651c16294573b2d19c6
wasif farid <wsffarid1@gmail.com>
2021-09-03 16:00:17 +0530

    swap 2 numbers

---
+++ SwapNumber/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SwapNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, temp;
+            Console.WriteLine("enter first number:");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter second number:");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            temp = num1;
+            num1 = num2;
+            num2 = temp;
+            Console.WriteLine("first number after swap is :" + num1);
+            Console.WriteLine("second number after swap is :" + num2);
+        }
+    }
+}
