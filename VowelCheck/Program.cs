a24930424fc29d32e6f0623e7145ba16e943eb05
wasif farid <wsffarid1@gmail.com>
2021-09-03 16:22:53 +0530

    vowel and consonent check

---
+++ VowelCheck/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace VowelCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an alphabet to check vowel or consonent:");
+            char alpha = Convert.ToChar(Console.ReadLine());
+            if (alpha == 'a' || alpha == 'e' || alpha == 'i' || alpha == 'o' || alpha == 'u' || alpha == 'A' || alpha == 'E' || alpha == 'I' || alpha == 'O' || alpha == 'U')
+            {
+                Console.WriteLine("enetered alphabet is a vowel");
+            }
+            else
+            {
+                Console.WriteLine("entered alphabet is a consonent");
+            }
+        }
+    }
+}
