bc90606cdcb989710b4a92fe1b76092f80aee278
wasif farid <wsffarid1@gmail.com>
2021-09-03 15:48:41 +0530

    quotient remainder added

---
+++ QuotientRemainder/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace QuotientRemainder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dividend , divisor ;
+            Console.WriteLine("enter the dividend:");
+            dividend = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter the divisor:");
+            divisor = Convert.ToInt32(Console.ReadLine());
+            int quotient = dividend / divisor;
+            int remainder = dividend % divisor;
+
+            Console.WriteLine("Dividend:{0} Divisor:{1}", dividend, divisor);
+            Console.WriteLine("Quotient = " + quotient);
+            Console.WriteLine("Remainder = " + remainder);
+        }
+    }
+}
